031f5841b4c29ca2ebbefcd8b96da0c3d8a6d616
Falling <135897083+F-alling@users.noreply.github.com>
2026-05-23 22:47:03 -0500

    Add files via upload

---
+++ wallpaper-foss/config.js
@@ -0,0 +1,29 @@
+window.CONFIG = {
+  // Base folder for all local assets
+  basePath: "file:///home/arch/Downloads/wallpaper-foss/",
+
+  // Backgrounds
+  wallpaper: "macos_wallpaper.png",
+  foreground: "macos_top_layer.png",
+
+  // Weather location
+  weatherLocation: {
+    latitude: 0,
+    longitude: 0,
+    elevation: 0
+  },
+
+  // Gallery images (just filenames now)
+  galleryImages: [
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
+    "5.jpg",
+    "6.jpg",
+    "7.jpg",
+    "8.jpg",
+    "9.jpg",
+    "0.jpg"
+  ]
+};
